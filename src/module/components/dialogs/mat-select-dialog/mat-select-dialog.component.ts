5e502630e53dc23cb0fe0f6055683cbeb25f06c9
Anthony Nahas <anthony.na@hotmail.de>
2019-04-09 22:04:48 +0200

    feat(package): added empty select dialog component

---
+++ src/module/components/dialogs/mat-select-dialog/mat-select-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mat-select-dialog',
+  templateUrl: './mat-select-dialog.component.html',
+  styleUrls: ['./mat-select-dialog.component.scss']
+})
+export class MatSelectDialog implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
