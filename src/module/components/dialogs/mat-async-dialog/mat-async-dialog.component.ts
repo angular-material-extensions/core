a48f8e76e8f11015415989e6689b00bde98529ac
Anthony Nahas <anthony.na@hotmail.de>
2019-04-09 22:03:44 +0200

    feat(package): added empty async dialog component

---
+++ src/module/components/dialogs/mat-async-dialog/mat-async-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mat-async-dialog',
+  templateUrl: './mat-async-dialog.component.html',
+  styleUrls: ['./mat-async-dialog.component.scss']
+})
+export class MatAsyncDialog implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
