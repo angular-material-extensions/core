3287d48209634e81007e39e4047672959f045b44
Anthony Nahas <anthony.na@hotmail.de>
2019-04-09 22:04:27 +0200

    feat(package): added empty loading dialog component

---
+++ src/module/components/dialogs/mat-loading-dialog/mat-loading-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mat-loading-dialog',
+  templateUrl: './mat-loading-dialog.component.html',
+  styleUrls: ['./mat-loading-dialog.component.scss']
+})
+export class MatLoadingDialog implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
