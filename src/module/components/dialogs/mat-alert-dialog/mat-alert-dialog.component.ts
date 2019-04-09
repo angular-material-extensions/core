000a7a7b1059ce76006b92bc3c27e93260789f68
Anthony Nahas <anthony.na@hotmail.de>
2019-04-09 20:00:49 +0200

    feat(package): added empty alert dialog

---
+++ src/module/components/dialogs/mat-alert-dialog/mat-alert-dialog.component.ts
@@ -0,0 +1,39 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+export interface MatAlertDialogData {
+  title?: string;
+  icon?: string;
+  type?: string;
+  message: string;
+  okTextButton?: string;
+}
+
+@Component({
+  selector: 'mat-alert-dialog',
+  templateUrl: './mat-alert-dialog.component.html',
+  styleUrls: ['./mat-alert-dialog.component.scss']
+})
+export class MatAlertDialog implements OnInit {
+
+  title: string;
+  icon: string;
+  type: string;
+  message: string;
+  okTextButton: string;
+
+  constructor(public dialogRef: MatDialogRef<MatAlertDialog>,
+              @Inject(MAT_DIALOG_DATA) public data: MatAlertDialogData) {
+  }
+
+  ngOnInit() {
+    if (this.data) {
+      this.title = this.data.title ? this.data.title : 'Alert';
+      this.icon = this.data.icon ? this.data.icon : 'warn';
+      this.type = this.data.type ? this.data.type : 'Confirm';
+      this.message = this.data.message ? this.data.message : 'Cancel';
+      this.okTextButton = this.data.okTextButton ? this.data.okTextButton : 'Ok';
+    }
+  }
+
+}
