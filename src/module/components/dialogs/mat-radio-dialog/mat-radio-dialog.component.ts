233819af9c70032de14485c1d2eee891b50ad435
Anthony Nahas <anthony.na@hotmail.de>
2019-04-09 22:04:38 +0200

    feat(package): added empty radio dialog component

---
+++ src/module/components/dialogs/mat-radio-dialog/mat-radio-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mat-radio-dialog',
+  templateUrl: './mat-radio-dialog.component.html',
+  styleUrls: ['./mat-radio-dialog.component.scss']
+})
+export class MatRadioDialog implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
