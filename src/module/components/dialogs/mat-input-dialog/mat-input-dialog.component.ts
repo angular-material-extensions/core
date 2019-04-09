29e19ea4fe2cba477c6e51da57a434a14ac86063
Anthony Nahas <anthony.na@hotmail.de>
2019-04-09 22:04:11 +0200

    feat(package): added empty input dialog component

---
+++ src/module/components/dialogs/mat-input-dialog/mat-input-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mat-input-dialog',
+  templateUrl: './mat-input-dialog.component.html',
+  styleUrls: ['./mat-input-dialog.component.scss']
+})
+export class MatInputDialog implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
