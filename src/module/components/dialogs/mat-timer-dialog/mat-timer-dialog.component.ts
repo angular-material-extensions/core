c06e6be5812970362db4f3a97abfb4187a6dc1f8
Anthony Nahas <anthony.na@hotmail.de>
2019-04-09 22:04:58 +0200

    feat(package): added empty timer dialog component

---
+++ src/module/components/dialogs/mat-timer-dialog/mat-timer-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mat-timer-dialog',
+  templateUrl: './mat-timer-dialog.component.html',
+  styleUrls: ['./mat-timer-dialog.component.scss']
+})
+export class MatTimerDialog implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
