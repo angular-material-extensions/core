3a2150d774a189e7489b09f0274549afaee62579
Anthony Nahas <anthony.na@hotmail.de>
2019-05-08 22:28:24 +0200

    feat(dialogs): added input dialog component

--- src/module/components/dialogs/mat-input-dialog/mat-input-dialog.component.ts
+++ src/module/components/dialogs/mat-input-dialog/mat-input-dialog.component.ts
@@ -1,12 +1,13 @@
 import {Component, Inject, OnInit, Optional} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {MAT_DIALOG_DATA, MatDialogRef, ThemePalette} from '@angular/material';
 
 export interface MatInputDialogData {
   title?: string;
+  color?: ThemePalette;
   icon?: string;
-  type?: string;
-  message: string;
-  okTextButton?: string;
+  appearance?: string;
+  placeholder?: string;
+  textButton?: string;
 }
 
 @Component({
@@ -16,11 +17,24 @@ export interface MatInputDialogData {
 })
 export class MatInputDialog implements OnInit {
 
+  title: string;
+  icon: string;
+  color: ThemePalette;
+  placeholder: string;
+  textButton: string;
+
   constructor(public dialogRef: MatDialogRef<MatInputDialog>,
               @Optional() @Inject(MAT_DIALOG_DATA) public data: MatInputDialogData) {
   }
 
   ngOnInit() {
+    if (this.data) {
+      this.title = this.data.title ? this.data.title : 'Enter the data';
+      this.icon = this.data.icon ? this.data.icon : 'edit';
+      this.color = this.data.color ? this.data.color : 'primary';
+      this.placeholder = this.data.placeholder ? this.data.placeholder : '';
+      this.textButton = this.data.textButton ? this.data.textButton : 'Ok';
+    }
   }
 
 }
